910f32c13438295c4b72433decf2ba5a3d3887de
Tolentino-JP <johnpaultolentino5@gmail.com>
2024-05-05 17:54:54 +0800

    Updated

--- public/js/login.js
+++ public/js/login.js
@@ -1,76 +1,38 @@
-// var mysql = require('mysql');
-
-// var establishConnection = require('./database');
-
-// var con = establishConnection();
-
-// new try create server
-
 const express = require("express");
+const bodyParser = require('body-parser');
 const app = express();
-
-app.use(express.static('../'));
-
-app.listen(3000, () => {
-  console.log("App listening on port 3000");
-})
+const mysql = require("mysql");
 
 
 
+var establishConnection = require('./database');
+var con = establishConnection();
 
+app.use(bodyParser.urlencoded({extended: false}));
 
+app.post('/login', (req, res) => {
+  const {username, password} = req.body;
 
-
-// const http = require('http')
-// const fs = require('fs')
-// const port = 8080
-
-// const server = http.createServer(function(req,res) {
-//   res.writeHead(200, {'Content-Type' : 'text/html' })
-//   fs.readFile("../html/login.html", function(error,data){
-//     if(error){
-//       res.writeHead(404)
-//       res.write('Error: File not found')
-//     }else{
-//       res.write(data)
-//     }
-//     res.end()
-//   })
-// })
-
-
-// server.listen(port, function(error){
-//   if(error){
-//     console.log("Something went wrong", error)
-//   }else{
-//     console.log("Server is listening on port "+ port)
-//   }
-// })
-
-
-
-
-
-
-
-
-
-
-
-
-// const loginForm = document.getElementById("login");
-
-// loginForm.addEventListener("submit", function(event) {
-//   event.preventDefault();
-
-//   const username = document.getElementById("username").value;
-//   const password = document.getElementById("password").value;
+  var sqlQuery = "SELECT first_name FROM users where first_name = " + username;
   
-//   if (username.trim() === "" || password.trim() === "") {
-//     alert("Please enter both username and password.");
-//     return;
-//   }
+  con.query(sql, function(err, result){
+    if(err){
+      throw err;
+    }else{
+      console.log(result.first_name);
+    }
+  })
+});
+
+
+
+const loginForm = document.getElementById("login");
+
+loginForm.addEventListener("submit", function(event) {
+  event.preventDefault();
   
-//   loginForm.reset();
-// });
+  return LogIn();
+  
+  loginForm.reset();
+});
   
--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const path = require('path');
 const bodyParser = require('body-parser');
-const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+const app = express();
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/login.html');
 });
 
 app.use(express.static('public'));
+
+
+
+
 
 // listen on port 3000
 app.listen(3000, () => {
